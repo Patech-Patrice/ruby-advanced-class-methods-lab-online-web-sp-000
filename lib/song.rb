a1345528a347a34f9c6962a2a92244e3d84d910c
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-29 17:06:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
     new_song #return new_song
   end
  
-  def self.create_from_filename(file_name)
+  def self.create_from_filename(file_name) #
     new_song = self.new
     new_song.name = file_name.split(" - ")[1].split(".")[0]
     new_song.artist_name = file_name.split(" - ")[0]
